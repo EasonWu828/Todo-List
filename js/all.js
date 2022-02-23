07f6a2f538f51aa04826f1782869736ed60227f7
Eason <alanwu0828@gmail.com>
2022-02-23 20:35:05 +0800

    完成 js 切換狀態功能、單一刪除功能

--- js/all.js
+++ js/all.js
@@ -1,34 +1,67 @@
 const inputText = document.querySelector('.inputText');
 const addBtn = document.querySelector('.addBtn');
-const allList = document.querySelector('.allList');
-const toDo = document.querySelector('.toDo');
-const finish = document.querySelector('.finish');
-const toDoItem = document.querySelector('.toDo-item');
-const clearItem = document.querySelector('.clear-item');
-const listItem = document.querySelector('.list-item');
-const closeBtn = document.querySelector('.close-btn');
-const work = document.querySelector('.work');
+const list = document.querySelector('.list');
+const deleteBtn = document.querySelector('.deleteBtn');
+const main = document.querySelector('.main');
 
 
 let data = [];
+// 切換狀態預設
+let toggleStatus = 'all';
 
-addBtn.addEventListener('click', addTodo);
+addBtn.addEventListener('click', e =>{
+  e.preventDefault();
+  addTodo();
+});
+// 鍵盤操作
+inputText.addEventListener('keypress', e =>{
+  if(e.key === 'Enter'){
+    addTodo()
+  }
+});
+// 監聽 Tab
+main.addEventListener('click', changeTab);
+
+// 切換狀態
+function changeTab(e){
+  toggleStatus = e.target.dataset.toggle;
+  let tabs = document.querySelectorAll('.main li');
+  tabs.forEach((item) => {
+    item.classList.remove('active');
+  })
+  e.target.classList.add('active');
+  updateList();
+}
+
+// checked 狀態更新資料
+function updateList(){
+  let showData = [];
+  if (toggleStatus === 'all') {
+    showData = data;
+  } else if (toggleStatus === 'work') {
+    showData = data.filter((item) => item.checked === '');
+  } else {
+    showData = data.filter((item) => item.checked === 'checked');
+  }
+  render(showData);
+}
+
 
 // 初始資料
-function render(){
+function render(todo){
   let str = '';
-  data.forEach((item, index) => {
-    str += `<li class="work">
-            <span class="material-icons check-box">
-              check_box_outline_blank
-            </span>
-            <p class="list-txt">${item.value}</p>
-              <span class="material-icons close-btn" data-num="${index}">
-                close
-              </span>
-        </li>`
+  todo.forEach((item, index) => {
+    str += `<li data-id="${item.id}">
+            <label for="" class="checkBox">
+              <input type="checkbox" ${item.checked}>
+              <p>${item.value}</p>
+            </label>
+            <a href="#"><span class="material-icons deleteBtn" data-num="${index}">
+              close
+            </span></a>
+          </li>`
   });
-  listItem.innerHTML = str;
+  list.innerHTML = str;
 }
 
 // 新增資料
@@ -38,20 +71,37 @@ function addTodo(){
     return;
   };
   let obj = {};
-  obj.value = inputText.value;
+  obj.value = inputText.value,
+  obj.id = new Date().getTime(),
+  obj.checked = '';
   data.push(obj);
   inputText.value = '';
-  render();
+  render(data);
 }
 
-// listItem.addEventListener('click', e => {
-//   let num = e.target.dataset.num;
-//   console.log(num);
-//   if (e.target.getAttribute("class") == "check-box") {
-//     console.log(123);
-//   }
-//   console.log(data);
-//   render();
-// })
+
+list.addEventListener('click', e => {
+  let id = parseInt(e.target.closest('li').dataset.id);
+  if (e.target.classList.contains('deleteBtn')){
+    e.preventDefault();
+    let num = e.target.dataset.num;
+    if(confirm('確定刪除這個項目?')){
+      data.splice(num, 1);
+    }
+  }else{
+    data.forEach((item)=>{
+      if (item.id === id){
+        if (item.checked === 'checked'){
+          item.checked = '';
+        }else{
+          item.checked = 'checked';
+        }
+      }
+    })
+  }
+  render();
+})
+
+
 
 
